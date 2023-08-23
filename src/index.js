a6350c0e170fe24c8ea2f2a38afd9ede76850cfa
Jasmin Kassas <jasminkassas@gmail.com>
2023-08-23 17:17:50 -0400

    Fixing privacy config settings

--- src/index.js
+++ src/index.js
@@ -7,14 +7,18 @@ import * as Sentry from "@sentry/browser";
 
 Sentry.init({
   dsn: "https://7778b23c197161567ad862ea6a69e0af@o19635.ingest.sentry.io/4505756244508672",
-  integrations: [
-    new Sentry.Replay(),
-  ],
+
   // Session Replay
   replaysSessionSampleRate: 1.0, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
   replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
-  maskAllText: false,
-  blockAllMedia: false,
+
+  integrations: [
+    new Sentry.Replay({
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
+  ],
+
 });
 
 
